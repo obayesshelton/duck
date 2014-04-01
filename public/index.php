4a2fdc75b2307b0e6dd25a784556c889896e84d5
Oliver Bayes-Shelton <me@bayes-shelton.co.uk>
2014-04-01 15:13:53 +0100

    inital commit with basic stuff

---
+++ public/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+    <link href="http://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
+
+    <style type="text/css">
+
+        body {
+            font: 400 18px/1 'Kaushan Script', cursive;
+            background-color: #eeeeee;
+        }
+
+        .div {
+            width:270px;
+            height:150px;
+            position:absolute;
+            left:50%;
+            top:50%;
+            margin:-75px 0 0 -135px;
+        }
+
+        .imgBorder {
+            padding: 15px;
+            background-color: white;
+            box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);
+            -moz-box-shadow: 0 1px 2px rgba(34,25,25,0.4);
+            -webkit-box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);
+        }
+
+        p {
+            text-align: center;
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+    <div class="div">
+
+        <img src="duck.jpeg" class="imgBorder" width="272" height="280"/>
+
+        <p>HELP ME !</p>
+
+    </div>
+
+</body>
+</html>
