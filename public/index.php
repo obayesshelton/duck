2358fc65be2b9cb01917669883fcbcbcfc58e5d7
Stewart Anderson <git@stewart-anderson.co.uk>
2014-04-02 11:43:06 +0100

    Formatting Image layouts

--- public/index.php
+++ public/index.php
@@ -7,19 +7,23 @@
                 margin: 0;
                 padding: 0;
             }
-            body {
+            body{
+                margin-top: 30px;
                 font: 400 18px/1 'Kaushan Script', cursive;
                 background-color: #eeeeee;
                 text-align: center;
             }
             .polariods{
                 display: inline-block;
+                width: 530px;
                 margin: 20px auto;
             }
             .polariods li{
+                display: inline-block;
                 list-style: none;
                 margin: 20px auto;
                 padding: 15px;
+                text-align: center;
                 background-color: white;
                 box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);
                 -moz-box-shadow: 0 1px 2px rgba(34,25,25,0.4);
@@ -34,6 +38,7 @@
     </head>
 
     <body>
+        <h1>Where is my Duck</h1>
         <ul class="polariods">
             <li>
                 <img src="https://farm4.staticflickr.com/3748/13568241533_bf0588be3e.jpg">
