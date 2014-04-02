1f08a9e9e9cea23f11ac0679888b1e3da8229586
Stewart Anderson <stewartanderson@stewartanderson.local>
2014-04-02 10:19:05 +0100

    Adding second image with caption

--- public/index.php
+++ public/index.php
@@ -1,50 +1,48 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="http://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            *{
+                margin: 0;
+                padding: 0;
+            }
+            body {
+                font: 400 18px/1 'Kaushan Script', cursive;
+                background-color: #eeeeee;
+                text-align: center;
+            }
+            .polariods{
+                display: inline-block;
+                margin: 20px auto;
+            }
+            .polariods li{
+                list-style: none;
+                margin: 20px auto;
+                padding: 15px;
+                background-color: white;
+                box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);
+                -moz-box-shadow: 0 1px 2px rgba(34,25,25,0.4);
+                -webkit-box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);
+            }
+            .polariods span{
+                display: block;
+                margin-top: 10px;
+                text-align: center;
+            }
+        </style>
+    </head>
 
-    <link href="http://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
-
-    <style type="text/css">
-
-        body {
-            font: 400 18px/1 'Kaushan Script', cursive;
-            background-color: #eeeeee;
-        }
-
-        .div {
-            width:364px;
-            height:550px;
-            position:absolute;
-            left:50%;
-            top:50%;
-            margin:-275px 0 0 -182px;
-        }
-
-        .imgBorder {
-            padding: 15px;
-            background-color: white;
-            box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);
-            -moz-box-shadow: 0 1px 2px rgba(34,25,25,0.4);
-            -webkit-box-shadow: 0 1px 3px rgba(34, 25, 25, 0.4);
-        }
-
-        p {
-            text-align: center;
-        }
-
-    </style>
-
-</head>
-
-<body>
-
-    <div class="div">
-
-        <img src="https://farm6.staticflickr.com/5329/13568546474_b0c3b6313c.jpg" class="imgBorder"/>
-
-        <p>HELP ME !</p>
-
-    </div>
-
-</body>
+    <body>
+        <ul class="polariods">
+            <li>
+                <img src="https://farm8.staticflickr.com/7006/13577569215_0336a56de0.jpg">
+                <span>I wheeley need help!!</span>
+            </li>
+            <li>
+                <img src="https://farm6.staticflickr.com/5329/13568546474_b0c3b6313c.jpg">
+                <span>Help me!</span>
+            </li>   
+        </ul>
+    </body>
 </html>
