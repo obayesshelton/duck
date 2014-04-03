df358652815021212fd82e133836798419007357
Stewart Anderson <git@stewart-anderson.co.uk>
2014-04-03 12:00:17 +0100

    Adding cat image and caption

--- public/index.php
+++ public/index.php
@@ -41,6 +41,10 @@
         <h1>Where is my Duck?</h1>
         <ul class="polariods">
             <li>
+                <img src="https://farm8.staticflickr.com/7337/13589488975_2df58c5182.jpg">
+                <span>I am feline scared!</span>
+            </li>
+            <li>
                 <img src="https://farm4.staticflickr.com/3688/13589868954_bd996b9428.jpg">
                 <span>Hang in there</span>
             </li>
